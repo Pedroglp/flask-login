0b31e7f287e629362fb1f1bee13792c397725eba
pedroglp <pedro@ttburger.com>
2018-04-13 17:20:18 -0300

    adicionando dicas para parse do request

--- res/items.py
+++ res/items.py
@@ -1,9 +1,14 @@
 from config import mongo, Resource
+from bson.json_util import dumps, loads
 
 class Items(Resource):
     def get(self, itemId):
+        items = mongo.db.items
         return {'result': 'Teste'}
 
 class ItemsList(Resource):
     def get(self):
-        return {'result': 'List'}
+        items = mongo.db.items
+        result = dumps(items)
+        #check marshamallow
+        return {'result': result}
