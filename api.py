fed8606b8b633655927c8f4f0436a8e8f2c4a08f
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-17 09:20:00 -0300

    Troca de nome para app

--- api.py
+++
@@ -1,9 +0,0 @@
-from config import app, api
-from res import Items, ItemsList
-from flask_restful import Api
-
-api.add_resource(ItemsList, '/items')
-api.add_resource(Items, '/items/<string:itemId>')
-
-if __name__ == '__main__':
-    app.run(debug=True)
