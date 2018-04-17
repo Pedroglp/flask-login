572db008732e508a11cad0ec476917e54d495d29
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-04-17 14:06:29 -0300

    adding marshmallow

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask
 from settings import *
-from resources import Items, ItemsList
+from resources import ItemResource, ItemsListResource
 from flask_restful import Api
 from flask_mongoengine import MongoEngine
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-#change this depending of your stage level
+#change this depending on your stage level
 app.config.from_object(DevelopmentConfig)
 db = MongoEngine(app)
 api = Api(app)
+ma = Marshmallow(app)
 
-api.add_resource(ItemsList, '/items')
-api.add_resource(Items, '/items/<string:itemId>')
+api.add_resource(ItemsListResource, '/items')
+api.add_resource(ItemResource, '/items/<string:itemId>')
 
 if __name__ == '__main__':
     app.run()
